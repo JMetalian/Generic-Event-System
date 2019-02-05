d7fe929eb31837d724d8e792b41f9bd52faf92d5
Can Kozan <blackclimb34@windowslive.com>
2019-02-05 18:25:57 +0300

    3-Totally Complicated Branch

--- Assets/Scripts/GameEventListener.cs
+++ Assets/Scripts/GameEventListener.cs
@@ -2,9 +2,19 @@
 using UnityEngine.Events;
 
 
-public class GameEventListener : IListener<Listener>
+public class GameEventListener : MonoBehaviour,IListener<Listener>
 {
+    
+    public GameEvent<Listener> ev;
+    public UnityEvent<Listener> invokeThis;
+
+    private void OnEnable()
+    {ev.AddListener(this);}
+
+    private void OnDisable()
+    {ev.RemoveListener(this);}
+
     public void OnRaised(Listener t)
-    {
-    }
+    {invokeThis.Invoke(t);}
+    
 }
--- Assets/Scripts/Listener.cs
+++ Assets/Scripts/Listener.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
     public class Listener:MonoBehaviour
     {
+        private GameEventListener eventListener = new GameEventListener();
         public void OnRaised(Listener t)
         {
 

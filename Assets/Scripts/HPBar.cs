5d3c56c96d4e0feb996463c6ca97155930c295f9
ibrahim <r3apper1201@gmail.com>
2019-02-05 16:32:45 +0300

    my branch

---
+++ Assets/Scripts/HPBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPBar : MonoBehaviour
+{
+    public ScriptableFloat HP;
+    public ScriptableFloat Mana;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(HP.Value);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HP.subValue(10);
+        print(HP.Value);
+    }
+    
+}
---
+++ Assets/Scripts/ScriptableFloat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewFloat",menuName = "float")]
+public class ScriptableFloat : ScriptableObject{
+    public float Value;
+    public void subValue(float val){
+        if(Value >= val){
+            Value -= val;
+        }
+        else{
+            Value = 0;
+        } 
+
+    }
+}
+
+
